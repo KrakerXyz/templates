b0c6c5c9827e14a633050b01077370b8b8a8b752
Josh Kraker <josh@fulcrumpro.com>
2022-04-22 19:53:26 -0400

    Monaco 0.33

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -2,17 +2,24 @@
 <template>
    <div>
       Home
+      <div id="editor-ide-container" style="width:100%; height:800px"></div>
    </div>
 </template>
 
 <script lang="ts">
 
+import { useMonacoEditor } from '@/services/monacoEditor';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    props: {
-    },
     setup() {
+
+        const { content } = useMonacoEditor(
+            'editor-ide-container'
+        );
+
+        content.value = 'console.log(\'Hello World\');';
+
         return {};
     }
 });
---
+++ web/src/services/monacoEditor.ts
@@ -0,0 +1,118 @@
+import type * as monaco from 'monaco-editor';
+import {
+    getCurrentScope,
+    onMounted,
+    onScopeDispose,
+    ref,
+    Ref,
+    watch,
+} from 'vue';
+
+export function useMonacoEditor(
+    containerId: string,
+    config?: Partial<EditorConfig>
+): { content: Ref<string>; errorMarkers: Ref<monaco.editor.IMarker[]> } {
+    const content = ref('');
+    const errorMarkers = ref<monaco.editor.IMarker[]>([]);
+
+    let editor: monaco.editor.IStandaloneCodeEditor | undefined;
+
+    const scope = getCurrentScope();
+    if (!scope) {
+        throw new Error('useMonacoEditor called without a scope');
+    }
+
+    onMounted(() => {
+        (window as any).require(['vs/editor/editor.main'], function () {
+            const ideContainer = document.getElementById(containerId);
+            if (!ideContainer) {
+                console.error(`#${containerId} not found`);
+                return;
+            }
+
+            const thisMonaco: typeof monaco = (window as any).monaco;
+
+            if (config?.javascriptLib) {
+                for (const k of Object.getOwnPropertyNames(config.javascriptLib)) {
+                    const libUrl = `ts:filename/${k}.d.ts`;
+                    if (
+                        thisMonaco.languages.typescript.javascriptDefaults.getExtraLibs()[
+                            libUrl
+                        ]
+                    ) {
+                        continue;
+                    }
+                    const lib = config.javascriptLib[k];
+                    thisMonaco.languages.typescript.javascriptDefaults.addExtraLib(
+                        lib,
+                        libUrl
+                    );
+                    thisMonaco.editor.createModel(
+                        lib,
+                        'typescript',
+                        thisMonaco.Uri.parse(libUrl)
+                    );
+                }
+            }
+
+            editor = thisMonaco.editor.create(ideContainer, {
+                value: content.value,
+                language: 'javascript',
+                renderValidationDecorations: 'off',
+                theme: 'vs-dark',
+                wordWrap: 'on',
+                wrappingIndent: 'indent',
+                lineNumbers: 'on',
+            }) as monaco.editor.IStandaloneCodeEditor;
+
+            editor.getModel()?.updateOptions({ tabSize: 3 });
+
+            let isOutgoingValue = false;
+            editor.onDidChangeModelContent(() => {
+                if (!editor) {
+                    return;
+                }
+                const newContent = editor.getValue();
+                isOutgoingValue = true;
+                content.value = newContent;
+            });
+
+            scope.run(() => {
+                watch(
+                    content,
+                    (c) => {
+                        if (isOutgoingValue) {
+                            isOutgoingValue = false;
+                            return;
+                        }
+                        editor!.setValue(c);
+                    },
+                    { immediate: true }
+                );
+            });
+
+            const obs = new ResizeObserver(() => {
+                if (!editor) {
+                    return;
+                }
+                editor.layout();
+            });
+
+            obs.observe(ideContainer);
+
+            scope.run(() => {
+                onScopeDispose(() => {
+                    obs.disconnect();
+                });
+            });
+        });
+    });
+
+    return { content, errorMarkers };
+}
+
+export interface EditorConfig {
+   javascriptLib: {
+      [name: string]: string;
+   };
+}
